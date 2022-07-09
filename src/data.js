96f1b0115664b5079ecfc1d4d92788df1fcc809f
Mashalane Pheletso <89118208+PheletsoBear@users.noreply.github.com>
2022-07-09 13:31:35 +0200

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,36 @@
+import img1 from './images/img1.jpg';
+import img2 from './images/img2.jpg';
+import img3 from './images/img3.jpg';
+
+const data = {
+
+    productData: [{
+            id: 1,
+            img: img1,
+            tile: 'Juicy Grapes',
+            desc: '',
+            price: 46,
+
+        },
+        {
+            id: 2,
+            img: img2,
+            tile: 'Red Watermelon',
+            desc: '',
+            price: 56,
+
+        },
+        {
+            id: 2,
+            img: img3,
+            tile: 'Juicy orage',
+            desc: '',
+            price: 46,
+
+        }
+
+    ],
+
+};
+
+export default data;
